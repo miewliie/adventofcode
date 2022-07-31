460d5a26ee5732dc42e656902bd40a1c1ce99f95
Miewliie <miewliie@gmail.com>
2022-07-31 12:52:39 +0700

    day1:part2 compare sum of three-measurement sliding window

---
+++ day1/ocean2.py
@@ -0,0 +1,39 @@
+import csv
+# dept_list = [199,200,208,210,200,207,240,269,260,263]
+
+
+def read_dept_list():
+    f = open("dept_list.csv", "r")
+    data = list(csv.reader(f, delimiter='\t'))
+    return data
+
+
+def convert_to_int(data):
+    temp = int(data[0])
+    return temp
+
+
+def compare_dept(data):
+    i = 1
+    increase_dept_count = 0
+    while i < len(data):
+        if data[i-1] < data[i]:
+            increase_dept_count += 1
+        i += 1
+    print(increase_dept_count)
+
+
+def sum_each_three(dept_list):
+    i = 0
+    list_of_sum = []
+    while i < (len(dept_list)-2):
+        total_sum = convert_to_int(dept_list[i]) + convert_to_int(dept_list[i+1]) + convert_to_int(dept_list[i+2])
+        list_of_sum.insert(i, total_sum)
+        i += 1
+    return list_of_sum
+
+
+if __name__ == '__main__':
+    data = read_dept_list()
+    results = sum_each_three(data)
+    compare_dept(results)
